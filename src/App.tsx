f3ecb8ac0c6b1b8bce0d2e470bde9e34dad2ada0
vladmertvychenko <vladyslav.mertvychenko@y-tree.com>
2023-10-28 21:13:38 +0300

    Init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { MapContainer } from './components/MapContainer.tsx';
+
+function App() {
+    return (
+    <>
+     <MapContainer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MapContainer.tsx
@@ -0,0 +1,3 @@
+export const MapContainer: React.FC = () => {
+    return (<h1> Map container</h1>);
+}
