a484130c851173e6b253c456664a9db338548a7a
vladmertvychenko <vladyslav.mertvychenko@y-tree.com>
2023-11-14 21:23:22 +0200

    Changed config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     base: '/powerkit-map/',
+    build: {
+        rollupOptions: {
+            output: {
+                dir: 'dist',
+                entryFileNames: 'powerMap.js',
+                assetFileNames: 'powerMap.css',
+                chunkFileNames: "powerMapChunk.js",
+                manualChunks: undefined,
+            }
+        }
+    },
     plugins: [react()],
 })
